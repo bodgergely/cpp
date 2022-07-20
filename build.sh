4172b4af24e8caaf4a1aed93a3a6e0c88f9b865a
Greg Bod <gergely.bod@hp.com>
2022-07-20 23:57:53 +0200

    sfsd

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 DIR='/c/Users/gergely.bod/workspace/cpp'
-# clang++ -std=c++20 -g -O3 -march=native -Wall -Wextra -Werror -Wpedantic -Wshadow -Wconversion -o $DIR/cpp/main.exe $DIR/cpp/main.cpp
-g++ -std=c++17 -g -O0 -march=native -Wall -Wextra -Wno-sign-compare -o $DIR/cpp/main.exe $DIR/cpp/main.cpp
+# clang++ -std=c++20 -g -O3 -march=native -Wall -Wextra -Werror -Weffc++ -Wpedantic -Wshadow -Wconversion -o $DIR/cpp/main.exe $DIR/cpp/main.cpp
+g++ -std=c++17 -g -O0 -march=native -Wall -Wextra -Werror -Wpedantic -Wno-sign-compare -o $DIR/cpp/main.exe $DIR/cpp/main.cpp
 # gcc -std=c99 -g -O0 -march=native -o $DIR/cpp/main.exe $DIR/cpp/main.c
