a564b0cfb4d9f806e325b7ac3229cb9a9df37a60
Greg Bod <gergely.bod@hp.com>
2022-10-12 22:13:14 +0100

    changing .

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-DIR='/c/Users/gergely.bod/workspace/cpp'
-# clang++ -std=c++20 -g -O3 -march=native -Wall -Wextra -Werror -Weffc++ -Wpedantic -Wshadow -Wconversion -o $DIR/cpp/main.exe $DIR/cpp/main.cpp
-g++ -std=c++17 -g -O0 -march=native -Wall -Wextra -Werror -Wpedantic -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -o $DIR/cpp/main.exe $DIR/cpp/main.cpp
-# gcc -std=c99 -g -O0 -march=native -o $DIR/cpp/main.exe $DIR/cpp/main.c
+DIR="$HOME/workspace/cpp"
+# clang++ -std=c++20 -g -O3 -march=native -Wall -Wextra -Werror -Weffc++ -Wpedantic -Wshadow -Wconversion -o $DIR/cpp/main $DIR/cpp/main.cpp
+g++ -std=c++17 -g -O0 -march=native -Wall -Wextra -Werror -Wpedantic -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -o $DIR/cpp/main $DIR/cpp/main.cpp
+# gcc -std=c99 -g -O0 -march=native -o $DIR/cpp/main $DIR/cpp/main.c
