652f866189ebe1cebae5d8e1ee1935a4226e98c7
Greg Bod <gergely.bod@hp.com>
2023-03-10 01:15:36 +0100

    fdg

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,16 +1,13 @@
-﻿
-//#include <UserEnv.h>
-// #include <Windows.h>
+﻿//#include <UserEnv.h>
+//#include <Windows.h>
+//#include "utils.h"
 #include <iostream>
 #include <vector>
-
-#include "utils.h"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-    pair<int, string> name{45, "Geri"};
-    cout << name << endl;
+    cout << "Hello World!\n";
     return 0;
 }
